c59478a43a9dc201feec1f7d8ac025f767fef20d
dna737 <adnanhere27@gmail.com>
2023-01-14 12:54:17 +0530

    enable saving functionality

--- lib/script.rb
+++ lib/script.rb
@@ -1,8 +1,10 @@
+require 'yaml'
+
 module Input
   def ask_user_input(valid_options)
     input = gets.chomp.downcase
     until valid_options.include?(input)
-      puts "Please enter 'p' to play a new game and 'x' to load a previous game."
+      puts "Please choose an option from: #{valid_options}"
       input = gets.chomp.downcase
     end
     input
@@ -22,10 +24,21 @@ class Decider include Input
     Game.new(contents).begin_game
   end
 
+  def retrieve_files
+    files = Dir["./saved_games/*"]
+    files.each_with_index{|file, index| puts "#{index+1}. #{file.partition("/").last.partition("/").last}"}
+    files
+  end
+
   def load_game
     if Dir.exist?("saved_games")
       #TODO: implement a feature that lets you load from a list of previous games.
       puts "Please select the game you want to load and play!"
+      files = retrieve_files
+      options = []
+      for i in 1..files.size do options.push(i.to_s) end
+      input = ask_user_input(options)
+      puts(Marshal.load(File.read("#{files[input.to_i-1]}")).load_words)
     else
       puts "No saved games found. Please enter 'p' to start a new game or 'q' to quit the program."
       input = ask_user_input([].push('p').push('q'))
@@ -45,13 +58,17 @@ end
 
 class Game include Input
   private attr_accessor :mistakes, :choices
-  private attr_reader :answer
+  private attr_reader :answer, :words
   def initialize(words)
     @words = words    
     @answer = select_word(words)
     @mistakes = 0
     @choices = []
     draw_hangman(mistakes)
+  end
+
+  def self.load_words
+    words
   end
 
   def select_word(words)
@@ -139,10 +156,24 @@ class Game include Input
     end
   end
 
+  #Returns true if the user wants to save the game; false otherwise.
   def save_or_continue
     puts "Press 's' if you want to 'S'ave the game and 'c' if you want to 'C'ontinue."
-
+    input = ask_user_input([].push('s').push('c'))
+    puts "\e[H\e[2J"
+    input == 's'
   end 
+
+  def save_game
+    file_name = Time.now
+    if Dir.exist?("./saved_games")
+      File.open("./saved_games/#{file_name}", 'wb') { |f| f.write(Marshal.dump(self)) }
+    else 
+      Dir.mkdir("./saved_games")
+      File.open("./saved_games/#{file_name}", 'wb') { |f| f.write(Marshal.dump(self)) }
+    end
+    puts "Your game has now been saved as #{file_name}!"
+  end
 
   def begin_game
     puts "Welcome to the Hangman game! You have 7 lives to guess the word!"
@@ -150,13 +181,20 @@ class Game include Input
       puts draw_hangman(mistakes)
       display_puzzle
       break if save_or_continue
+      puts draw_hangman(mistakes)
+      display_puzzle
       input_array = seek_input
       check_input(input_array[0], input_array[1])
       puts "\e[H\e[2J"
       break if end_game?
     end
-    puts draw_hangman(mistakes)
-    final_message
+
+    if end_game?
+      puts draw_hangman(mistakes)
+      final_message
+    else
+      save_game  
+    end
   end
 
   def draw_hangman(index)
